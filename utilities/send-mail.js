5bab3f411c3b7db84708b3d78870290a48b6dedc
chenanga <38496970+chenanga@users.noreply.github.com>
2022-04-21 21:29:42 +0800

    Update send-mail.js

--- utilities/send-mail.js
+++ utilities/send-mail.js
@@ -154,9 +154,10 @@ ${$.load(text.replace(/<img.*?src="(.*?)".*?>/g, "\n图片: $1\n").replace(/<br>
     .then(function (response) {
       if (response.status === 200 && response.data.errcode === 0) {
         console.log('已获取accesstoken:',response.data.access_token)
-        console.log('已获取accesstoken:',accessToken)
+        
 
         var  accessToken = response.data.access_token
+        console.log('已获取accesstoken:',accessToken)
         console.log('已获取accesstoken')
       } else {
         console.warn('获取accesstoken失败', response.data)
